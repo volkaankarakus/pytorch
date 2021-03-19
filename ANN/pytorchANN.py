b55d8f2bfe50e5517698c35ee0218584eaf76260
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-03-20 00:46:13 +0300

    Update pytorchANN.py

--- ANN/pytorchANN.py
+++ ANN/pytorchANN.py
@@ -12,10 +12,67 @@ Created on Fri Mar 19 14:18:31 2021
     # Bunun icin de hidden layerlar eklememiz gerekiyor.
     
 #%% Import Libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
+from torch.autograd import Variable # gradient hesabi icin
+from torch.utils.data import DataLoader #train ve test veri setini pytorch icin kullanilabilir hale gelir.
 
+#%% Prepare Dataset
+# load data
+train = pd.read_csv('train.csv',dtype = np.float32)
+
+# split data into features(pixels) and labels(numbers from 0 to 9)
+targets_numpy = train.label.values # bu kisim class label(targetlar)
+features_numpy = train.loc[:,train.columns != "label"].values/255 # normalization
+
+# train test split. Size of train data is 80% and size of test data is 20%. 
+features_train, features_test, targets_train, targets_test = train_test_split(features_numpy,
+                                                                             targets_numpy,
+                                                                             test_size = 0.2,
+                                                                             random_state = 42) 
+
+# create feature and targets tensor for train set. As you remember we need variable to accumulate gradients. Therefore first we create tensor, then we will create variable
+featuresTrain = torch.from_numpy(features_train) #numpy'dan Tensor'e dondu.
+targetsTrain = torch.from_numpy(targets_train).type(torch.LongTensor) # data type is long
+
+# create feature and targets tensor for test set.
+featuresTest = torch.from_numpy(features_test)
+targetsTest = torch.from_numpy(targets_test).type(torch.LongTensor) # data type is long
+
+# batch_size, epoch and iteration
+"""
+batch_size, bizim verimizi kaca bolerek train edecegimiz.
+    1000 train veri setinden olusan elemani 10'lu gruplarsak herbir grubun icinde 100 tane eleman olur.batch_size=100
+    
+epoch, kac kere train edildigi.epoch=5 icin ayni veri setini 5 kere train edicem demektir.
+    loss azalana kadar train etmeliyiz.
+"""
+batch_size = 100
+n_iters = 10000
+num_epochs = n_iters / (len(features_train) / batch_size) # num_epochs=n_iters/len(group_size)
+num_epochs = int(num_epochs)
+
+# Pytorch train and test sets
+train = torch.utils.data.TensorDataset(featuresTrain,targetsTrain) # elimizdeki datasetini tensor data setine cevirir.
+test = torch.utils.data.TensorDataset(featuresTest,targetsTest) #
+
+# data loader (elimizdeki veri seti ile sample'larimizin combine edilmesini saglar ve multiprocess yapmamizi saglar.Bu da sureci hizlandirir.)
+train_loader = DataLoader(train, batch_size = batch_size, shuffle = False)
+test_loader = DataLoader(test, batch_size = batch_size, shuffle = False)
+
+# visualize one of the images in data set
+plt.imshow(features_numpy[10].reshape(28,28))
+plt.axis("off")
+plt.title(str(targets_numpy[10]))
+plt.savefig('graph.png')
+plt.show()
 #%% Create ANN Model
 class ANNModel(nn.Module):
     
@@ -69,9 +126,68 @@ output_dim = 10
 model = ANNModel(input_dim, hidden_dim, output_dim)
 
 # Cross Entropy Loss 
-error = nn.CrossEntropyLoss()
+error = nn.CrossEntropyLoss() # yukariya softmax eklememiz gerekiyordu ama zaten bu CrossEntropyLoss'un icinde var.o yuzden eklemedik.
 
-# SGD Optimizer
+#%% SGD Optimizer
 learning_rate = 0.02
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
  
+# ANN model training
+count = 0
+loss_list = []
+iteration_list = []
+accuracy_list = []
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+
+        train = Variable(images.view(-1, 28*28))
+        labels = Variable(labels)
+        
+        # Clear gradients
+        optimizer.zero_grad()
+        
+        # Forward propagation
+        outputs = model(train)
+        
+        # Calculate softmax and ross entropy loss
+        loss = error(outputs, labels)
+        
+        # Calculating gradients
+        loss.backward()
+        
+        # Update parameters
+        optimizer.step()
+        
+        count += 1
+        
+        if count % 50 == 0:
+            # Calculate Accuracy         
+            correct = 0
+            total = 0
+            # Predict test dataset
+            for images, labels in test_loader:
+
+                test = Variable(images.view(-1, 28*28))
+                
+                # Forward propagation
+                outputs = model(test)
+                
+                # Get predictions from the maximum value
+                predicted = torch.max(outputs.data, 1)[1]
+                
+                # Total number of labels
+                total += len(labels)
+
+                # Total correct predictions
+                correct += (predicted == labels).sum()
+            
+            accuracy = 100 * correct / float(total)
+            
+            # store loss and iteration
+            loss_list.append(loss.data)
+            iteration_list.append(count)
+            accuracy_list.append(accuracy)
+        if count % 500 == 0:
+            # Print Loss
+            print('Iteration: {}  Loss: {}  Accuracy: {} %'.format(count, loss.data, accuracy))
+            
